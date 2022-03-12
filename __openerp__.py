7e2e5390856e67b4d01d6d9453734858fe8af2e7
le minh tu <leminhtu0908dtd@gmail.com>
2022-03-12 12:19:06 +0700

    create/player/management

---
+++ __openerp__.py
@@ -0,0 +1,15 @@
+#noi dung cua file mo ta
+{
+    "name":"Player Management", #ten cua module
+    "version": "1.0",
+    "author": "levinh",
+    "description": "This module is used to manage player", #mo ta module
+    "website": "",
+    "category":"General",
+    "depends":["base"],#khai bao nhung module lien quan
+    "init_xml":[],
+    "demo_xml":[],
+    "update_xml":["player_view.xml","player_menu.xml"],
+    "active":False,
+    "installable":True,
+}
---
+++ player.py
@@ -0,0 +1,21 @@
+#coding=utf-8
+from openerp.osv import fields,osv
+
+class player (osv.osv):
+    _name='player.player'
+    _columns = {
+        #cac thuoc tinh cua lop player
+        'name':fields.char('Tên cầu thủ', size=25, required=True, translate=True),
+        'portfolio':fields.integer('Tổng số danh hiệu'),
+        'coach':fields.char('HLV',size=100, required=True, translate=True),
+        'pos':fields.selection([('td','Tiền đạo'),('tv','Tiền vệ'),
+                                 ('hv','Hậu vệ'),('gk','Thủ môn')],'Vị trí'),
+        'birthday':fields.datetime('Ngày sinh'),
+        'active':fields.boolean('Đang hoạt động ?'),
+        'notes':fields.text('Lưu ý')
+    }
+    _defaults={
+        'portfolio':0,
+        'active':True
+    }
+player() #tao 1 the hien cho lop player_player
